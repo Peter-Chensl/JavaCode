3359f179381015d4b7f832bb5050d932fda30e3f
Peter-Chensl <930847702@qq.com>
2020-04-19 18:29:56 +0800

    Java学习

---
+++ Random1.java
@@ -0,0 +1,11 @@
+public class Random1{
+	public static void main(String[] args){
+		/*double random = Math.random();
+		System.out.println("random = " + random);
+		int random1 = (int)(Math.random()*10);
+		System.out.println("random1 = " + random1);*/
+		int random2 = (int) (Math.random()*90+10);
+		System.out.print("random2 = " + random2);
+	}
+
+}
