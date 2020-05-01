6bee71da42a21df50379a33de465bc9c000f644a
Peter-Chensl <930847702@qq.com>
2020-05-01 23:52:53 +0800

    Java学习

---
+++ day_05_01/src/day_05_01/test.java
@@ -0,0 +1,12 @@
+package day_05_01;
+
+public class test {
+	public static void main(String[] args){
+		int sum = 0,i;
+		for(i = 0; i < 5;i++) {
+			sum += i;
+		}
+		System.out.println("sum = " + sum);
+		System.out.print("i = " + i);
+	}
+}
