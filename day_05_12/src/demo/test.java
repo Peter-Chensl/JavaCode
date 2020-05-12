122e9f0e2518ee4f0b3710c4615503ea29cdc032
Peter-Chensl <930847702@qq.com>
2020-05-12 23:11:23 +0800

    Java

---
+++ day_05_12/src/demo/test.java
@@ -0,0 +1,16 @@
+package demo;
+
+import java.util.Arrays;
+
+public class test {
+	public static void main(String[] args) {
+		int [] array = {1,2,3,4,5};
+		/*for(int i = 0;i < array.length;i++) {
+			System.out.print(array[i] + " ");
+		}*/
+		/*for(int in:array) {
+			System.out.print(in + " ");
+		}-*/
+		System.out.print(Arrays.toString(array));
+	}
+}
